69cfefb7cb3905eba73ef3d172a158adb0614ec9
Jayant Sharma <105975938+JayantSharma17@users.noreply.github.com>
2024-03-20 13:32:59 +0530

    deploy change3

--- controller/auth.js
+++ controller/auth.js
@@ -70,7 +70,7 @@ const userLogin = async (req, res) => {
                 console.log(emailExist)
                 const token = await emailExist.generateAuthToken();
                 console.log(`Token: ${token}`);
-                res.status(200).json({ message: "User tLogin successfully", response: emailExist, token: token });
+                res.status(200).json({ message: "User Login successfully", response: emailExist, token: token });
             }
             else {
                 res.status(400).json({ message: "Invalid Credentials p" });
@@ -112,7 +112,7 @@ const memberLogin = async (req, res) => {
                 console.log(emailExist)
                 const token = await emailExist.generateAuthToken();
                 console.log(`Token: ${token}`);
-                res.status(200).json({ message: "Member Login successfully", response: emailExist, token: token });
+                res.status(200).send({ message: "Member Login successfully", response: emailExist, token: token });
             }
             else {
                 res.status(400).json({ message: "Invalid Credentials p" });
