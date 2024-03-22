99561752245ae52bc2c0e3c2a5617f7f49c81195
Jayant Sharma <105975938+JayantSharma17@users.noreply.github.com>
2024-03-22 16:47:04 +0530

    ongoing event api 1

--- router/event.js
+++ router/event.js
@@ -3,7 +3,7 @@ const { createEvent, ongoingEvent } = require('../controller/event');
 const router = express.Router();
 
 router.post('/create-event/:userId', createEvent);
-router.post('/ongoing-event/:userId', ongoingEvent);
+router.get('/ongoing-event/:userId', ongoingEvent);
 
 
 module.exports = router;
